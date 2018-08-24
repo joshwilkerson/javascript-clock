21901622f215720fcc9d4c811d74508d9056b980
Josh Wilkerson <jgwilkerson@gmail.com>
2018-08-23 22:07:46 -0500

    updated scripts

--- main.js
+++ main.js
@@ -1,63 +1,37 @@
-// clock face elements
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 
-// get starting time value (on page load)
-const now = new Date();
-const seconds = now.getSeconds();
-const minutes = now.getMinutes();
-const hours = now.getHours();
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const minutes = now.getMinutes();
+	const hours = now.getHours();
 
+	// add degrees for hour hand (in between main markers)
+	const degreesBetween = 30;
+	const betweenHourPercent = minutes / 60 * 100;
+	const additionalDegrees = degreesBetween * betweenHourPercent / 100;
 
-// set initial hands position
-let secondsDegrees = ((seconds / 60) * 360) + 90;
-secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	// seconds
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-let minutesDegrees = ((minutes / 60) * 360) + 90;
-minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	// minutes
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-let hoursDegrees = (((hours / 12) * 360) + 90) + (minutes*.5);
-hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-
-// minute hand (triggered when second hand gets to top)
-function moveMinuteHand() {
-	minutesDegrees += 6;
-	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;	
-}
-
-
-// hour hand (triggered when second hand gets to top)
-// incremented by .5 degree to give appearance of continual movement
-function moveHourHand(){
-	hoursDegrees += .5;
+	// hours
+	const hoursDegrees = ((hours / 12) * 360) + 90 + additionalDegrees;
 	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-}
 
-
-// putting it all together
-function clockMovement() {
-	// second hand movement
-	secondsDegrees += 6;
-	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;	
-	
-	// 1 increment prior to top
-	if(secondsDegrees == 444) {
-		// remove CSS transition to prevent movement jump
-		secondHand.classList.add('reset');
-		// reset second hand degree value
-		secondsDegrees = 84;
-		// increment minute and hour hands
-		moveMinuteHand();
-		moveHourHand();
+	function addMovement() {
+		document.querySelector('.clock-face').classList.add('canMove');
 	}
-		
-	// re-apply CSS transition property
-	if(secondsDegrees == 96) {
-		secondHand.classList.remove('reset');
-	}	
+
+	setTimeout(addMovement, 100);
 }
 
-setInterval(clockMovement, 1000);
+setDate();
+setInterval(setDate, 1000);
